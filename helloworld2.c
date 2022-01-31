d582ba47c1c272635d752e117b9608aa8fbec4ea
j-press <jeremiah.mathers@gmail.com>
2022-01-30 22:54:45 -0600

    First Commit

---
+++ helloworld2.c
@@ -0,0 +1,16 @@
+/*
+* hello2.c
+* My first C program with comments.
+* by Jeremiah Mathers
+* created 2022/01/30
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf( "Hello, world!\n");
+    return 0;
+}
+
+/* eof */
